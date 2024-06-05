0263c544c03e573ed914e79d1c058cb189530388
JennGuz <143773875+JennGuz@users.noreply.github.com>
2024-06-05 17:46:07 +0000

    latest changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,69 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+
+
+
+const TrafficLight = () => {
+
+	const [color, setColor] = useState("red");
+	const [intervalId, setIntervalId] = useState(null);
+
+	const changeColor = () => {
+
+		if(intervalId === null) {
+
+			const newIntervalId = setInterval(() => {
+
+				setColor(currentColor => {
+					
+					if(currentColor === 'red') {
+						return 'yellow';
+
+					}else if(currentColor === 'yellow') {
+						return 'green';
+
+					}else if(currentColor === 'green') {
+						return 'red';
+					}
+
+				});
+
+			}, 1000);
+
+			setIntervalId(newIntervalId);
+
+		}else {
+			clearInterval(intervalId);
+			setIntervalId(null);
+		}
+	}
+	
+	console.log(color);
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="trafficLight">
+				<div className="trafficTop">h</div>
+				<div className="container">
+					<div className={color === 'red' ? 'red active' : 'red'}>
+
+					</div>
+					<div className={color === 'yellow' ? 'yellow active' : 'yellow'} >
+						
+					</div>
+					<div className={color === 'green' ? 'green active' : 'green'}>
+						
+					</div>
+				</div>
+				<button className="button" onClick={changeColor}>On/Off</button>
+			</div>warning: You appear to have cloned an empty repository.
+
+		</>
 	);
 };
 
-export default Home;
+export default TrafficLight;
